0349679a8bea744bba5d931a4131f9f1c83e2031
leocaca123 <132609393+leocaca123@users.noreply.github.com>
2024-05-01 18:53:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    juanmetemela: "analmente",
+    penenegro: "mecos",
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
